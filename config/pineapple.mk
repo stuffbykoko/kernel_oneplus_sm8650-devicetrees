6c31fdaa296f582445d3a3468fd8f3425d6be8d3
Ridhi Shah <ridhshah@qti.qualcomm.com>
2022-07-14 15:13:43 -0700

    ARM: dts: msm: Add camera devicetree for pineapple
    Add camera dtsi support for pineapple target.
    
    CRs-Fixed: 3244380
    Change-Id: I0ce9fcbf01d278a429f14c4068dcb531e08efcee

---
+++ config/pineapple.mk
@@ -0,0 +1,4 @@
+dtbo-$(CONFIG_ARCH_PINEAPPLE)	:= pineapple-camera.dtbo
+dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-sensor-cdp.dtbo \
+									pineapple-camera-sensor-mtp.dtbo \
+									pineapple-camera-sensor-qrd.dtbo
