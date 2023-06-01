b323ddf83fd2fb89e52ad350ee4bfb1811ea59c6
Titus Abraham <tabraham@qti.qualcomm.com>
2023-06-01 12:51:15 -0700

    Revert "ARM: dts: msm: camera: v2 overlay change to handle ipe lowsvs clk change"
    This reverts commit f395866f26515aff2cbfb2672e5dba3200959db2.
    
    Change-Id: Ic8f125b01e7b6fb448758b7616853c6f0860facf

--- config/pineapple.mk
+++ config/pineapple.mk
@@ -1,5 +1,4 @@
 dtbo-$(CONFIG_ARCH_PINEAPPLE)	:= pineapple-camera.dtbo
-dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-overlay-v2.dtbo \
-									pineapple-camera-sensor-cdp.dtbo \
+dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-sensor-cdp.dtbo \
 									pineapple-camera-sensor-mtp.dtbo \
 									pineapple-camera-sensor-qrd.dtbo
